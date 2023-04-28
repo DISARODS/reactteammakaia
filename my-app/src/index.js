bf3c22f36d440c4033f641ae6e48bcf18012244b
DISARODS <dieguisam@gmail.com>
2023-04-28 15:55:50 -0500

    creando las funciones de cada una de las secciones del proyecto

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+function Header() {
+  return (
+    <header>
+      <h1>IMMERSIRVE EXPERIENCES THAT DELIVER</h1>
+    </header>
+  );
+}
+
+function Main (){
+  return (
+    <main>
+      <h1>THE LEADER IN INTERACTIVE VR</h1>
+      <p>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations hace transformed bussiness though digital experiences that bind to their brand</p>
+    </main>
+  );
+}
+
+function Footer (){
+  return  (
+    <footer>
+      <h4>OUR CREATIONS</h4>
+    </footer>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header/>
+    <Main/>
+    <Footer/>
+    {/* <App />  */}
+    {/* QUITAR APP AL FINALIZAR  */}
   </React.StrictMode>
 );
 
