4470b94a53f77ffe2db798eb965c86b768995f0d
paola torres <paolandrea96@gmail.com>
2023-05-02 23:31:55 -0500

    Main Design

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,23 +7,24 @@ import reportWebVitals from './reportWebVitals';
 //--------------------new styled footer-------------------
 
 import styled from 'styled-components';
+import ImageMain from "./assets/images/desktop/main/image-interactive.jpg";
 
 //--------------------const footer-------------------
 
 const images = require.context('./assets/images/desktop/footer', true);
 const imageText = {
   // asignar el texto a cada imagen por nombre 
-  'image-deep-earth.jpg':'DEEP EARTH',
-  'image-night-arcade.jpg':'NIGHT ARCADE',
-  'image-soccer-team.jpg':'SOCCER TEAM VR',
-  'image-grid.jpg':'THE GRID',
-  'image-from-above.jpg':'FROM UP ABOVE VR',
-  'image-pocket-borealis.jpg':'POCKET BOREALIS',
-  'image-curiosity.jpg':'THE CURIOSITY',
-  'image-fisheye.jpg':'MAKE IT FISHEYE',
+  'image-deep-earth.jpg': 'DEEP EARTH',
+  'image-night-arcade.jpg': 'NIGHT ARCADE',
+  'image-soccer-team.jpg': 'SOCCER TEAM VR',
+  'image-grid.jpg': 'THE GRID',
+  'image-from-above.jpg': 'FROM UP ABOVE VR',
+  'image-pocket-borealis.jpg': 'POCKET BOREALIS',
+  'image-curiosity.jpg': 'THE CURIOSITY',
+  'image-fisheye.jpg': 'MAKE IT FISHEYE',
 };
 
-//--------------------const footer-------------------
+//--------------------const main-------------------
 
 function Header() {
   return (
@@ -33,14 +34,18 @@ function Header() {
   );
 }
 
-function Main (){
-  return (
-    <main>
-      <h1>THE LEADER IN INTERACTIVE VR</h1>
-      <p>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations hace transformed bussiness though digital experiences that bind to their brand</p>
-    </main>
-  );
-}
+
+// function mainContainer() {
+//   return (
+//     <main>
+//       <img src=""> </img>
+//       <div>
+//       <h1>THE LEADER IN INTERACTIVE VR</h1>
+//       <p>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations hace transformed bussiness though digital experiences that bind to their brand</p>
+//       </div>
+//     </main>
+//   );
+// }
 
 //--------------------styled components for new footer------------------
 const FooterContainer = styled.footer`
@@ -76,6 +81,47 @@ const CreationsContainer = styled.div`
     }
   }
 `;
+const MainContainer = styled.main`
+background-color: #fff;
+padding:4%;
+justify-content: center;
+align-items: center;
+  img{
+    height: 30%;
+    width: 30%;
+    position: relative;
+  }
+  div{
+    padding: 2.5% 2% 1.5% 4%;
+    width: 22%;
+    height: 17%;
+    position: absolute;
+    top: 300px;
+    left: 470px;
+    background-color: #fff;
+    display:flex;
+    flex-direction: column;
+    font-weight: lighter;
+   
+  
+    h1 {
+      font-size: 800;
+      margin-bottom: 20px;
+      height: 400px;
+      width:60%;
+      font-weight: lighter;
+      line-height: 29px;
+      color:black;
+    }
+    p{
+      height: 800px;
+      width:65%;
+      color: grey;
+      font-size: x-small;
+    }
+  }
+    `;
+
 //-------------------- new footer-------------------
 function Footer() {
   return (
@@ -97,7 +143,16 @@ function Footer() {
     </FooterContainer>
   );
 }
-
+function Main() {
+  return (
+    <MainContainer>
+      <img src={ImageMain} alt="interactuve play"></img>
+      <div>
+        <h1>THE LEADER IN INTERACTIVE VR</h1>
+        <p>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations hace transformed bussiness though digital experiences that bind to their brand</p>
+      </div>
+</MainContainer>
+)}
 //-------------------- new footer-------------------
 //--------------------footer old-------------------
 
@@ -129,20 +184,20 @@ function Footer() {
 //--------------------final part-------------------
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Header/>
-    <Main/>
-    <Footer/>
-    {/* <App />  */}
-    {/* QUITAR APP AL FINALIZAR  */}
-  </React.StrictMode>
-);
+  root.render(
+    <React.StrictMode>
+      <Header />
+      <Main />
+      <Footer />
+      {/* <App />  */}
+      {/* QUITAR APP AL FINALIZAR  */}
+    </React.StrictMode>
+  );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
 
 
 
