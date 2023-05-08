2af0c7bb02deee747df0c805a3b50c342001c71f
DISARODS <dieguisam@gmail.com>
2023-05-07 22:57:49 -0500

    agregando

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import styled from 'styled-components';
+import styledF from 'styled-components';
+import styled, { css } from 'styled-components';
+
+
+// -------------import header------------------
+import React, { useState } from 'react';
 
 // -------------import images header------------------
-
-// -------------import images main------------------
+import ImageHeader from "./assets/images/desktop/header/image-hero.jpg";
+import ImageHeaderomobil from './assets/images/mobile/header/image-hero.jpg';
+import LogoHeader from './assets/images/logo.svg';
+import MenuHamburguesa from './assets/images/mobile/header/icon-hamburger.svg';
+import LogoMobile from './assets/images/mobile/header/logomobile.svg';
+import CloseMenu from "./assets/images/mobile/header/icon-close.svg";
 
 // -------------import images footer------------------
 
@@ -17,37 +26,260 @@ import instagramIcon from './assets/images/icon-instagram.svg';
 import logoicon from './assets/images/logo.svg';
 
 
+//--------------------const header-------------------
+const list = [
+  {
+    texto: "About",
+    link: "#About"
+  },
+  {
+    texto: "Careers",
+    link: "#Careers"
+  },
+  {
+    texto: "Events",
+    link: "#Events"
+  },
+  {
+    texto: "Products",
+    link: "#Products"
+  },
+]
+
+//--------------------styled components header------------------
+
+const BannerHeader = styled.header`
+ h1 {
+  position: absolute;
+  font-size: 80px;
+  font-style: normal;
+  font-weight: lighter;
+  top: 20%;
+  padding: 30px 50px;
+  border: 3px solid white;
+  margin-left: 100px;
+  color: white;
+  width: 600px;
+  line-height: 1.0;
+}
+
+img {
+  width: 100%;
+  height: 30%;
+}
+
+nav {
+  width: 130%;
+  position: absolute;
+  top: 70px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: white;
+  padding: 18px 18px 18px 65px;
+  gap: 60px;
+  
+}
+
+ul{
+  color: white;
+  list-style: none;
+  display: flex;
+  gap: 40px;
+}
+
+li a{
+  text-decoration: none;
+  color: white;
+}
+
+ul li:hover{
+  border-bottom: 3px solid white;
+  cursor: pointer;
+}
+
+.imagen-mobile{
+  display: none;
+}
+
+.logo{
+  width: 10%;
+  position: relative;
+  left: 40px;
+}
+
+.logomobile{
+  display: none;
+}
+
+.fondohamburguesa{
+  display: none;
+  opacity:0;
+  .botonHamburguesa{
+    display: none;
+  }
+}
+${({ theme }) => css`
+    @media (max-width: 375px) {
+      header {
+        nav {
+          width: 70%;
+        }
+    
+        img {
+          display: none;
+        }
+        
+        .imagen-mobile{
+          display: flex;
+          width: 100%;
+          object-fit: cover;
+          position: relative;
+          z-index: -1000;
+        }          
+          
+        }
+        .logo{
+          width: 15%;
+          position: relative;
+          left: 40px;  
+          z-index: 1000;
+          display:none;
+        }
+        .logomobile{
+          display: block;
+          width: 40%;
+          margin-left: -30px;
+          z-index: 1000;
+        }
+        h1 {
+          position: absolute;
+          font-size: 40px;
+          font-style: normal;
+          font-weight: lighter;
+          top: 25%;
+          padding: 35px 50px;
+          border: 3px solid white;
+          margin-left: 7%;
+          color: white;
+          width: 200px;
+          line-height: 1.0;
+          z-index: -1;
+          
+          
+        }
+        ul {
+          display: none;
+        }
+      
+      }
+    
+    }
+  `}
+`;
+const HamburguesaHeader = styled.div`
+${({ theme }) => css`
+    @media (max-width: 375px) {
+       {
+        ul {
+          width: 90%;
+          margin-left: -297px;
+          height: 500%;
+          display: none;
+          top: -22px;
+          font-size: 20px;
+          text-transform: uppercase;
+          line-height: 2.5;
+          position: absolute;
+          background-color: black;
+          padding-top: 40%;
+          display: none;
+          z-index: 0;
+        }
+        .botonCerrar.visible {
+          display: block;
+        }
+        .fondohamburguesa{
+          display: block;
+          opacity:1;
+          background-color: transparent;
+          width: 40px;
+          position: relative;
+          top: -4px;
+          margin-right: -18px;
+          border: none;  
+          .botonHamburguesa{
+          width: 100%;
+          height: 20px;
+          display: block;
+        }
+      }
+    }
+   
+  `}
+`;
+
+//-------------------- function header-------------------
+
+function Header() {
+  const [menuAbierto, setMenuAbierto] = useState(false);
+
+  function toggleMenu() {
+    console.log('Se hizo clic en el botón');
+    setMenuAbierto(!menuAbierto);
+  }
+
+  return (
+    <BannerHeader>
+      <header>
+        <nav>
+          <img src={LogoMobile} alt='logomobile' className='logomobile'></img>
+          <img src={LogoHeader} alt='logo' className='logo'></img>
+          <ul>
+            {list.map(item => <li><a href={item.link}>{item.texto}</a></li>)}
+          </ul>
+
+          <HamburguesaHeader>
+            <div>
+              <ul style={{ display: menuAbierto ? 'block' : 'none' }}>
+                {list.map(item => <li><a href={item.link}>{item.texto}</a></li>)}
+              </ul>
+              <button className='fondohamburguesa' onClick={toggleMenu}>
+                {!menuAbierto &&
+                  <img src={MenuHamburguesa} alt='hamburguesa' className='botonHamburguesa'></img>
+                }
+                <img src={CloseMenu} alt='close' className={`botonCerrar ${menuAbierto ? 'visible' : ''}`} onClick={toggleMenu}></img>
+              </button>
+            </div>
+          </HamburguesaHeader>
+        </nav>
+        <div>
+          <img src={ImageHeaderomobil} className='imagen-mobile' alt="imagen hero mobil"></img>
+          <img src={ImageHeader} alt="imagen hero"></img>
+          <div>
+            <h1>IMMERSIRVE EXPERIENCES THAT DELIVER</h1>
+          </div>
+        </div>
+      </header>
+    </BannerHeader>
+
+  );
+}
+
+// -------------import images main------------------
+
+
+
 //--------------------const footer-------------------
 
 const images = require.context('./assets/images/desktop/footer', true);
 const imagesmob = require.context('./assets/images/mobile/footer', true);
 
-
-//--------------------const footer-------------------
-
-function Header() {
-  return (
-    <header>
-      <h1>IMMERSIRVE EXPERIENCES THAT DELIVER</h1>
-    </header>
-  );
-}
-
-function Main (){
-  return (
-    <main>
-      <h1>THE LEADER IN INTERACTIVE VR</h1>
-      <p>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations hace transformed bussiness though digital experiences that bind to their brand</p>
-    </main>
-  );
-}
-
 //--------------------styled components for new footer------------------
-const FooterContainer = styled.footer`
+const FooterContainer = styledF.footer`
 `;
 
-
-const HeadContainerF = styled.div`
+const HeadContainerF = styledF.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -68,8 +300,9 @@ const HeadContainerF = styled.div`
   }
 
   button {
-    background-color: #000;
-    color: #fff;
+    
+    background-color: #fff;
+    color: #000;
     font-size: 1.2rem;
     font-weight: 500;
     border: none;
@@ -77,18 +310,26 @@ const HeadContainerF = styled.div`
     border-radius: 5px;
     cursor: pointer;
     margin-left: auto;
+    border: 1px solid black;
   }
 
   button:hover {
-    background-color: #fff;
-    color: #000;
-    border: 1px solid black;
-
+    background-color: #000;
+    color: #fff;
   }
+
+  @media (max-width: 375px) {
+    .headerf {
+      justify-content: center;
+    }
+
+    .hide-on-mobile {
+      display: none;
+    }
+
 `;
 
-
-const CreationsContainer = styled.div`
+const CreationsContainer = styledF.div`
   text-align: center;
   padding-left: 3.5%;
   padding-right: 3.5%;
@@ -162,30 +403,48 @@ const CreationsContainer = styled.div`
     margin-top: 20px;
   }
 
+  button {
+    background-color: #fff;
+    color: #000;
+    font-size: 1.2rem;
+    font-weight: 500;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 3px;
+    cursor: pointer;
+    justify-content: center;
+    border: 1px solid black;
+  }
+
+  button:hover {
+    background-color: #000;
+    color: #fff;
+  }
+
   @media (max-width: 375px) {
     .hide-on-mobile {
       display: none;
     }
     
-    .hide-on-desktop {
-      display: block;
+    .botonsito{
+      padding-top: 10%;
+      padding-bottom: 30%;
     }
+    
   }
 
   @media (min-width: 376px) {
     .hide-on-mobile {
       display: block;
-      
     }
-    
+
     .hide-on-desktop {
       display: none;
     }
   }
 `;
 
-const EnderContainer = styled.section`
-
+const EnderContainer = styledF.section`
 section {
   background-color: black;
   color: #ccc;
@@ -195,7 +454,6 @@ section {
   justify-content: space-between;
   width: 100%; /* Ancho al 100% para ocupar todo el espacio disponible */
   margin-top: 2%;
-  
 }
 
 .left {
@@ -265,9 +523,52 @@ section {
 
   align-self: flex-end;
 }
+
+@media (max-width: 375px) {
+  section{
+    flex-direction: column;
+    align-items: center;
+  }
+
+
+  .left img{
+    margin-left: 20%;
+    margin-right: 8%;
+    width: 60%;
+    padding-top: 30%;
+  }
+
+  .menu-links{
+    flex-direction: column;
+    padding-top: 12%;
+    padding-bottom: 17%;
+  }
+
+  .menu-links a{
+    padding-top: 12%;
+    padding-bottom: 17%;
+  }
+
+  .right{
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-left: 10%;
+    padding-bottom: 15%;
+  }
+
+  .footer-end p{
+    align-items: center;
+justify-content: center;
+  }
+}
+
+@media (min-width: 376px) {
+  
+}
 `;
 
-//-------------------- new footer-------------------
+//-------------------- function footer-------------------
 function Footer() {
 
   const imageText = {
@@ -288,7 +589,7 @@ function Footer() {
       <HeadContainerF>
       <div className='headerf'>
         <h2>OUR CREATIONS</h2>
-        <button>SEE ALL</button>
+        <button className='hide-on-mobile'>SEE ALL</button>
       </div>
       </HeadContainerF>
 
@@ -321,6 +622,10 @@ function Footer() {
             );
           })}
         </div>
+
+        <div className='botonsito'>
+          <button className='hide-on-desktop'>SEE ALL</button>
+        </div>
       </div>
       </CreationsContainer>
 
@@ -346,7 +651,7 @@ function Footer() {
                   <a href="#"><img src={instagramIcon} alt="Instagram"></img></a>
               </div>
               <div className="footer-end">
-                    <p>All rights reserved &copy; 2023 TuEmpresa.</p>
+                    <p>All rights reserved &copy; 2023</p>
               </div>
             </div>
             
@@ -378,7 +683,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StyledStrictMode>
     <Header/>
-    <Main/>
+    {/* <Main/> */}
     <Footer/>
     {/* <App />  */}
     {/* QUITAR APP AL FINALIZAR  */}
