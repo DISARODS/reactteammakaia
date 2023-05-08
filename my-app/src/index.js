90120071124df8660247d9d5208ed1a1072ca09f
DISARODS <dieguisam@gmail.com>
2023-05-08 14:18:48 -0500

    preparando lo final

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,3 +1,4 @@
+
 import ReactDOM from 'react-dom/client';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -820,6 +821,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
 
 
 
